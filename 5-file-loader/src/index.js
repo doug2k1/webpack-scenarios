fca0057707a1a1438f9a833040dbda3bddee433b
Douglas Matoso <doug2k1@gmail.com>
2018-10-06 21:17:36 -0300

    Update example 5 to webpack 4

--- 5-file-loader/src/index.js
+++ 5-file-loader/src/index.js
@@ -1,6 +1,8 @@
-import cow from './cow'
-import './sass/main.scss'
-import icon from './img/icon.png'
+import cow from "./cow";
+import "./sass/main.scss";
+import icon from "./img/icon.png";
 
-document.querySelector('#box').innerText = cow.say('Webpack can handle all kinds of files!')
-document.querySelector('#icon').innerHTML = `<img src="${icon}" />`
+document.querySelector("#box").innerText = cow.say(
+  "Webpack can handle all kinds of files!"
+);
+document.querySelector("#icon").innerHTML = `<img src="${icon}" />`;
